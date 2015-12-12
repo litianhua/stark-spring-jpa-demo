6343238765367e50fef18c2bcf00536694f706e2
litianhua <sky.lith@outlook.com>
2015-12-12 17:38:02 +0800

    add annotations
    添加一些代码注释

--- src/com/stark/demo/specifications/UserSpecs.java
+++ src/com/stark/demo/specifications/UserSpecs.java
@@ -24,17 +24,19 @@ public class UserSpecs {
 			public Predicate toPredicate(Root<User> root, CriteriaQuery<?> query,
 					CriteriaBuilder builder) {
 				Predicate p = null;
+				//判断参数Map是否为空
 				if (paramMap != null && !paramMap.isEmpty()) {
 					if(paramMap.containsKey("key") && paramMap.get("key") != null){
 						String key = "%" + paramMap.get("key") + "%";
 						Path<String> _p1 = root.get("name");
 						Path<String> _p2 = root.get("description");
+						//生成对应的条件，语句为：(name LIKE key OR description LIKE key)
 						p = builder.or(builder.like(_p1, key), builder.like(_p2, key));
 					}
 				}
-				if(p == null){
+				if(p == null){//如果以上没有相关的条件，就直接生成条件，为：(state = '1')
 					p = builder.equal(root.get("state"), "1");
-				}else{
+				}else{//如果已有相关的条件，就以上的条件为基础生成，为：(state = '1') AND (name LIKE key OR description LIKE key)
 					p = builder.and(builder.equal(root.get("state"), "1"), p);
 				}
 				if(p == null){
